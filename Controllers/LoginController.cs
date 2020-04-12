ea59da2ab2f82900953082a8f9298a28da5579fc
Muhammad Bilal <Muhammad.Bilal@uhs.ae>
2020-04-12 10:22:35 +0400

    Signed-off-by: Muhammad Bilal <Muhammad.Bilal@uhs.ae>

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWTAuthAPI.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWTAuthAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+        private IConfiguration _config;
+        public LoginController(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        [HttpGet]
+        public IActionResult Login(string UserName , string Password)
+        {
+            UserModel login = new UserModel();
+            login.UserName = UserName;
+            login.Password = Password;
+
+            IActionResult response = Unauthorized();
+
+            var user = AuthenticateUser(login);
+
+            if(user != null)
+            {
+                var tokenstr = GenerateJSONWebToken(user);
+                response = Ok(new { token = tokenstr });
+            }
+            return response;
+        }
+
+        [Authorize]
+        [HttpPost("Post")]
+        public string Post()
+        {
+            var identity = HttpContext.User.Identity as ClaimsIdentity;
+            IList<Claim> claim = identity.Claims.ToList();
+
+            var userName = claim[0].Value;
+            return "Welcome To " + userName;
+        }
+
+        [Authorize]
+        [HttpGet("GetValue")]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+            return new string[] { "Value1", "Value2", "Value3" };
+        }
+        private string GenerateJSONWebToken(UserModel userInfo)         
+        {
+            var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["JWTToken:Key"]) );   
+            var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(JwtRegisteredClaimNames.Sub , userInfo.UserName),
+                new Claim(JwtRegisteredClaimNames.Sub , userInfo.Password),
+                new Claim(JwtRegisteredClaimNames.Jti , Guid.NewGuid().ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _config["JWTToken:Issuer"],
+                audience: _config["JWTToken:Issuer"],
+                claims,
+                expires: DateTime.Now.AddMinutes(120),
+                signingCredentials: credentials
+                );
+
+            var encodetoken = new JwtSecurityTokenHandler().WriteToken(token);
+            return encodetoken;
+        }
+
+        public UserModel AuthenticateUser(UserModel login)
+        {
+            UserModel user = null;
+            if(login.UserName == "bilal" && login.Password == "bilal")
+            {
+                user = new UserModel { UserName = "bilal", Password = "bilal", Email = "muhammad.bilal23593@gmail.com" };
+            }
+
+            return user;
+        }
+    }
+}
